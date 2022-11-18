f2fe1e489dc99854b636be93a626ec85defe6e7a
Neto Daniel ZINGA <danielnzinga5@gmail.com>
2022-11-18 19:46:07 +0100

    Update gameboard

--- script.js
+++ script.js
@@ -35,3 +35,47 @@ const gameBoard = (() => {
 
     return { setField, getField, reset };
 })();
+
+const displayController = (() => {
+    const fieldElements = document.querySelectorAll(".field");
+    const messageElement = document.querySelector("#message");
+    const restartButton = document.querySelector("#restart-button");
+
+    fieldElements.forEach((field) => 
+    field.addEventListener("click", (e) => {
+        if(gameController.getIsOver() || e.target.textContent !== "") return;
+        gameController.playRound(parseInt(e.target.dataset.index));
+        updateGameboard();
+    })
+    );
+
+    restartButton.addEventListener("click", (e) => {
+        gameBoard.reset();
+        gameController.reset();
+        updateGameboard();
+        setMessageElement("Player X's turn");
+    });
+
+    const updateGameboard = () => {
+        for (let i = 0; i < fieldElements.length; i++) {
+            fieldElements[i].textContent = gameBoard.getField(i);
+            
+        }
+    };
+
+
+    const setResultMessage = (winner) => {
+        if (winner == "Draw") {
+            setMessageElement("It's a draw!");
+        } else {
+            setMessageElement(`Player ${winner} has won!`);
+        }
+    };
+
+
+    const setMessageElement = (message) => {
+        messageElement.textContent = message;
+    }
+
+    return { setResultMessage, setMessageElement };
+})();
