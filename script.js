d7b26c70de5c0a17bf63ee995e3027e194a54e07
Neto Daniel ZINGA <danielnzinga5@gmail.com>
2022-11-18 19:14:45 +0100

    set up the methode getField and setField to get and set the sign received from player

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+const Player = (sign) => {
+    this.sign = sign;
+
+
+    const getSign = () => {
+        return sign;
+    };
+
+
+    return { getSign };
+};
+
+const gameBoard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];
+    
+    const setField = (index, sign) => {
+        if (index > board.length) return;
+        return board[index];
+    };
+
+
+    const getField = (index) => {
+        if (index > board.length) return;
+        return board[index];
+    };
+
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    };
+
+    return { setField, getField, reset };
+})();
